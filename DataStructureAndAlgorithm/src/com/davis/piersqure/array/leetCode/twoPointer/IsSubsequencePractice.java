05719ff8d49d43a3606b97088746d87dfefcab02
dvsnayak <yello.davis@gmail.com>
2024-04-02 23:52:22 +0530

    leed code two pointer problem aded

---
+++ DataStructureAndAlgorithm/src/com/davis/piersqure/array/leetCode/twoPointer/IsSubsequencePractice.java
@@ -0,0 +1,23 @@
+package com.davis.piersqure.array.leetCode.twoPointer;
+
+public class IsSubsequencePractice {
+	// s = "abc", t = "ahbgdc" true
+	// s = "axc", t = "ahbgdc" false
+	public static boolean isSubsequence(String s, String t) {
+		int i = 0;
+		int j = 0;
+		while (i < s.length() && j < t.length()) {
+			if (s.charAt(i) == t.charAt(j)) {
+				i++;
+			}
+			j++;
+		}
+		return i == s.length();
+	}
+
+	public static void main(String[] args) {
+		String s = "", t = "c";
+		System.out.println(isSubsequence(s, t));
+	}
+
+}
